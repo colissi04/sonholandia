0812bfaf8d22ccebcff07b5f92631a79966b4fce
colissi04 <rcolissi@gmail.com>
2024-07-09 22:48:59 -0300

    upload do projeto

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import { Text, SafeAreaView } from "react-native";
+
+export default function Index() {
+  return (
+    <SafeAreaView>
+      <Text>
+        Hello World!!
+      </Text>
+    </SafeAreaView>
+  );
+}
